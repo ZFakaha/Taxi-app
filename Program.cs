cbefd6a5212a55565125d0d61fa6ef28dc1ac965
ZFakaha <zulfakaha@gmail.com>
2022-03-23 00:56:45 +0700

    upload project taxi-app (class & object C#)

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectTaxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek dari kelas taxi
+            Taxi Driver1 = new Taxi();
+
+            // mengeset nilai properties
+            Driver1.DriverName = "Zuka";
+            Driver1.OnDuty = true;
+            Driver1.NumPassenger = 10;
+
+            // pemanggilan method
+            Driver1.TaxiInfo();
+            Driver1.PickUpPassenger();
+            Driver1.DropOffPassenger();
+       
+            Console.ReadKey();
+        }
+    }
+}
